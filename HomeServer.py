bb01e1488b4827a249dc937bc49e32ce6cec32b5
Tyler Brady <tbbrady33@gmail.com>
2025-05-05 11:24:50 -0600

    now able to exit the server with ctrl-c

--- HomeServer.py
+++ HomeServer.py
@@ -1,36 +1,53 @@
 import socket
 import sys
+import signal
+
+# Define the signal handler to exit gracefully
+def signal_handler(sig, frame):
+    print('Exiting gracefully...')
+    sys.exit(0)
+
+# Set up the signal handler for graceful exit on Ctrl+C
+signal.signal(signal.SIGINT, signal_handler)
 
 Host = 'localhost'  # The server's hostname or IP address
-Port = 5000        # The port used by the server
+Port = 5000         # The port used by the server
 
 # Create a TCP/IP socket that closes on its own when the program ends
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-
     # Bind the socket to the address and port
-    server_address = (Host,Port)
+    server_address = (Host, Port)
     print('starting up on %s port %s' % server_address, file=sys.stderr)
     sock.bind(server_address)
 
-    #listen for incoming connections by setting the socket to listen mode
+    # Listen for incoming connections
     sock.listen(1)
 
-    while True:
-        # Wait for a connection
-        print('waiting for a connection', file=sys.stderr)
-        connection, client_address = sock.accept()
-        try:
-            print('connection from', client_address, file=sys.stderr)
+    # Set a timeout to periodically allow signal handling
+    sock.settimeout(1.0)  # Set the timeout to 1 second
 
-            # Receive the data in small chunks and retransmit it
-            with connection:
-                print(f"Connected by {client_address}")
-                while True:
-                    data = connection.recv(1024)
-                    if not data:
-                        break
-                    print(f"Received: {data.decode()}")
-                
-        finally:
-            # Clean up the connection
-            connection.close()
+    print('waiting for a connection', file=sys.stderr)
+
+    while True:
+        try:
+            # Wait for a connection (with timeout)
+            connection, client_address = sock.accept()
+            try:
+                print('connection from', client_address, file=sys.stderr)
+
+                # Receive the data in small chunks and retransmit it
+                with connection:
+                    print(f"Connected by {client_address}")
+                    while True:
+                        data = connection.recv(1024)
+                        if not data:
+                            break
+                        print(f"Received: {data.decode()}")
+
+            finally:
+                # Clean up the connection
+                connection.close()
+
+        except socket.timeout:
+            # Timeout occurred, just continue the loop
+            pass
--- MachineDataSender.py
+++ MachineDataSender.py
@@ -2,7 +2,7 @@ import socket
 import sys
 
 HOST = 'localhost'  # The server's hostname or IP address
-PORT = 10000        # The port used by the server
+PORT = 5000        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     server_address = (HOST, PORT)
